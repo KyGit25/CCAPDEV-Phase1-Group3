ce9cbdee0b81302c93c62b0f99d153586f5d8768
Kyle Maristela <161435410+KyGit25@users.noreply.github.com>
2025-07-14 17:04:04 +0800

    Update app.js

--- Phase 2/app.js
+++ Phase 2/app.js
@@ -1,2 +1,5 @@
 const authRoutes = require('./routes/auth');
 app.use('/auth', authRoutes);
+
+const reservationRoutes = require('./routes/reservation');
+app.use('/reservation', reservationRoutes);
